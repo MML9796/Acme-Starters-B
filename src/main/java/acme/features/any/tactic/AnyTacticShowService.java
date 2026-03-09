9d830ef518fc8e44d64eafa29a0d1b803647f2cb
MFC1728 <juagompoz@alum.us.es>
2026-03-09 12:42:10 +0100

    S3 List and Display Strategies (#39)

---
+++ src/main/java/acme/features/any/tactic/AnyTacticShowService.java
@@ -0,0 +1,36 @@
+
+package acme.features.any.tactic;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.client.components.principals.Any;
+import acme.client.services.AbstractService;
+import acme.entities.tactic.Tactic;
+
+@Service
+public class AnyTacticShowService extends AbstractService<Any, Tactic> {
+
+	@Autowired
+	private AnyTacticRepository	repository;
+	private Tactic				tactic;
+
+
+	@Override
+	public void load() {
+		int id;
+
+		id = super.getRequest().getData("id", int.class);
+		this.tactic = this.repository.findOneTacticById(id);
+	}
+
+	@Override
+	public void authorise() {
+		super.setAuthorised(true);
+	}
+
+	@Override
+	public void unbind() {
+		super.unbindObject(this.tactic, "name", "expectedPercentage", "kind", "notes");
+	}
+}
--- src/main/resources/WEB-INF/views/fragments/menu.jsp
+++ src/main/resources/WEB-INF/views/fragments/menu.jsp
@@ -18,6 +18,11 @@
 
 <acme:menu-bar>
 	<acme:menu-left>
+	
+		<acme:menu-option code="master.menu.strategy">
+			<acme:menu-suboption code="master.menu.strategy.list" action="/any/strategy/list"/>
+		</acme:menu-option>
+		
 		<acme:menu-option code="master.menu.anonymous" access="isAnonymous()">
 			<acme:menu-suboption code="master.menu.anonymous.favourite-link" action="http://www.example.com/"/>
 		</acme:menu-option>
