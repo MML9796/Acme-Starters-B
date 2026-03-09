3640f51e42cd66d6453916609c11e081e4edc547
Francisco Javier Parra  Gamez <frapamez@gmail.com>
2026-03-09 15:43:15 +0100

    Añadidos @Service faltantes refs: #41

--- src/main/java/acme/features/any/inventor/AnyInventorShowService.java
+++ src/main/java/acme/features/any/inventor/AnyInventorShowService.java
@@ -2,11 +2,13 @@
 package acme.features.any.inventor;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import acme.client.components.principals.Any;
 import acme.client.services.AbstractService;
 import acme.realms.Inventor;
 
+@Service
 public class AnyInventorShowService extends AbstractService<Any, Inventor> {
 
 	//Internal state
