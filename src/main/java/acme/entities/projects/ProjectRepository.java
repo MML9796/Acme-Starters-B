dbd76d95b80564e90a63cf581be7b6cf6e5f657c
josbardel <josbardel1@alum.us.es>
2026-05-01 05:03:02 +0200

    Project Entity

---
+++ src/main/java/acme/entities/projects/ProjectRepository.java
@@ -0,0 +1,27 @@
+
+package acme.entities.projects;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import acme.client.repositories.AbstractRepository;
+import acme.entities.campaign.Campaign;
+import acme.entities.invention.Invention;
+import acme.entities.strategy.Strategy;
+
+@Repository
+public interface ProjectRepository extends AbstractRepository {
+
+	@Query("select i from Invention i where i.project.id = :id")
+	List<Invention> getProjectInventionsById(int id);
+	@Query("select c from Campaign c where c.project.id = :id")
+	List<Campaign> getProjectCampaignsById(int id);
+	@Query("select s from Strategy s where s.project.id = :id")
+	List<Strategy> getProjectStrategiesById(int id);
+
+	@Query("select count(pm) from ProjectMember pm where pm.project.id = :id")
+	Integer getProjectMemberCountById(int id);
+
+}
